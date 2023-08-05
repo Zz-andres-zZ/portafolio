0181d2a485257e22b80dd993036a3096b1caf5c3
Andrés Jh'ndz <96813041+Zz-andres-zZ@users.noreply.github.com>
2023-08-05 17:55:25 -0400

    Update message_controller.php

--- app/controller/message_controller.php
+++ app/controller/message_controller.php
@@ -23,15 +23,12 @@ class Mailto {
         $this->mail->SMTPSecure = 'ssl';                             //Enable SMTP authentication
         //$this->mail->SMTPDebug = SMTP::DEBUG_SERVER;                 //Enable verbose debug output
         $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;       //Enable implicit TLS encryption
-
-        //$this->mail->Host = 'smtp.hostinger.com';              //Set the SMTP server to send through
-        //$this->mail->Port = 465;                               //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
         
-        $this->mail->Host = 'smtp.hostinger.com';              //Set the SMTP server to send through
-        $this->mail->Port = 465;                               //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        $this->mail->Host = '';              //Set the SMTP server to send through
+        $this->mail->Port = ;                                   //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
         
-        $this->mail->Username = 'andresjhm.2440@couchwmt.com';  //SMTP username
-        $this->mail->Password = 'Developer1@couchwmt.com';     //SMTP password
+        $this->mail->Username = '';  //SMTP username
+        $this->mail->Password = '';     //SMTP password
     
     }
 
